9d3b9fafcf3b9e68d230e2ed41fd17a3d29fcd38
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2014-11-02 20:15:04 -0800

    iniiti

---
+++ README.md
@@ -0,0 +1,17 @@
+Flask API to provide MBSP breakdowns of sentence.
+
+Checkout this repo.
+
+sudo pip install virtualenv
+cd this repo
+
+virtualenv env
+
+source env/bin/activate
+
+pip install flask
+
+Download MBSP library from http://www.clips.ua.ac.be/pages/MBSP and unzip in this dir
+
+start it up with:
+python chompywurds.py
---
+++ chompywurds.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+import MBSP
+
+
+@app.route("/")
+def hello():
+    return "Chompy wurd - post me sum content..\n"
+
+@app.route("/", methods=['POST'])
+def chomp_wurds():
+    wurds = request.json['wurds']
+
+    print "Got wurds: " + wurds
+    breakdown = MBSP.parse(wurds)
+    return jsonify({'orig': wurds, 'breakdown': breakdown}), 201
+
+if __name__ == "__main__":
+    app.run()
